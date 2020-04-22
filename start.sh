f63019d6f0309683b72c79d1770c7ef877d951b6
Brendan Quinlivan <brendan@calypsoai.com>
2020-04-21 20:21:19 -0700

    moving to relative calls

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-source /home/pi/PythonProjects/cat_sprayer/sprayer/bin/activate
-. /home/pi/PythonProjects/cat_sprayer/env_setup
-nohup python /home/pi/PythonProjects/cat_sprayer/cat_sprayer.py &
+source sprayer/bin/activate
+. env_setup
+nohup python cat_sprayer.py &
 deactivate
 
 
