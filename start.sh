faa1bd70c86bec1adaa9b6d4173f3a579ea9fde4
Brendan Quinlivan <brendan@calypsoai.com>
2020-04-21 20:16:20 -0700

    updating staring script

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 source /home/pi/PythonProjects/cat_sprayer/sprayer/bin/activate
 . /home/pi/PythonProjects/cat_sprayer/env_setup
 nohup python /home/pi/PythonProjects/cat_sprayer/cat_sprayer.py &
+deactivate
 
 
